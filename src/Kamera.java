6ee8f7b2bf9c5b4efdd8a74bbf5b72023a5484bb
McGam3r <98489481+McGam3r@users.noreply.github.com>
2025-02-12 23:43:31 +0100

    feat: camera reset reset camera with 'r'

--- src/Kamera.java
+++ src/Kamera.java
@@ -61,5 +61,10 @@ public class Kamera {
         }
     }
 
+    public void reset() {
+        kameraAbstand = 35;
+        kameraHohe = 15;
+        rotationsWinkel = 0;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -79,6 +79,10 @@ public class Main {
                 kamera.rotiere(kameraGeschwindigkeit * 2);
             }
 
+            if (tastatur.istGedrueckt('r')) {
+                kamera.reset();
+            }
+
             if (tastatur.istGedrueckt('w')) {
                 auto.beschleunigen();
             } else if (tastatur.istGedrueckt('s')) {
